280910ff22b3f754950ebf0d90169aa2c62aa209
alejandro <alejandrober96@gmail.com>
2023-06-08 16:02:26 -0500

    Add user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- get "about-us", to: "about#index", as: :about
+  get "about-us", to: "about#index", as: :about
 
-  # Defines the root path route ("/")
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
-  # root "articles#index"
+
 end
